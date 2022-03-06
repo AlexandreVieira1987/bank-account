60ef6a2be50ae7b92b55f4a14a41fcd3f9d78b66
Alexandre Vieira <alexandre@febacapital.com>
2022-03-06 08:59:21 -0300

    add inject dependecy in services

--- src/api/services/customer.service.ts
+++ src/api/services/customer.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import {CustomerEntity} from "../entities/customer.entity";
 import {getRepository, Repository} from "typeorm";
+import {InjectRepository} from "@nestjs/typeorm";
 
 @Injectable()
 export class CustomerService
 {
-    async model(): Promise<Repository<CustomerEntity>>
-    {
-        return getRepository(CustomerEntity)
-    }
+    constructor(
+        @InjectRepository(CustomerEntity)
+        private model: Repository<CustomerEntity>
+    ) {}
 
     async findAll(): Promise<CustomerEntity[]>
     {
-        return (await this.model()).find()
+        return await this.model.find()
     }
 
     async create(attributes: CustomerEntity): Promise<CustomerEntity | Error>
     {
-        const model = await this.model()
-
-        if (await model.findOne({cpf: attributes.cpf})) {
+        if (await this.model.findOne({cpf: attributes.cpf})) {
             return new Error('Customer already exists')
         }
 
-        const save = await model.save(attributes)
+        const save = await this.model.save(attributes)
 
         if (!save) {
             return new Error('Error on save customer')
