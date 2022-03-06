e44cfed3e5ad38a1d3ab7cd72911d00fea27d643
Alexandre Vieira <alexandre@febacapital.com>
2022-03-06 17:13:04 -0300

    adjusts test to account controller

--- src/api/controllers/account.controller.spec.ts
+++ src/api/controllers/account.controller.spec.ts
@@ -1,18 +1,86 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AccountController } from './account.controller';
+import {Test, TestingModule} from '@nestjs/testing';
+import {AccountController} from './account.controller';
+import {CustomerEntity} from "../entities/customer.entity";
+import {AccountService} from "../services/account.service";
+import {MovementService} from "../services/movement.service";
+import {CustomerService} from "../services/customer.service";
+import {TransferService} from "../services/transfer.service";
+import {getRepositoryToken} from "@nestjs/typeorm";
+import {MovementEntity} from "../entities/movement.entity";
+import {TYPE_OPERATION_CREDIT} from "../utils/constants";
+import {AccountEntity} from "../entities/account.entity";
+import {TransferEntity} from "../entities/transfer.entity";
+
+const accountMock: AccountEntity[] = [
+    new AccountEntity({customer_id: 1, balance: 1000}),
+    new AccountEntity({customer_id: 2, balance: 1000}),
+    new AccountEntity({customer_id: 3, balance: 1000}),
+]
+
+const customerMock = new CustomerEntity({name: 'Testing', cpf: '12154125428', id: undefined})
+const movementMock = new MovementEntity({type: TYPE_OPERATION_CREDIT, value: 1000, account_id: 1})
 
 describe('AccountController', () => {
-  let controller: AccountController;
+    let accountController: AccountController;
+    let accountService: AccountService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AccountController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                AccountService,
+                MovementService,
+                CustomerService,
+                TransferService,
+                {
+                    provide: getRepositoryToken(MovementEntity),
+                    useValue: {
+                        find: jest.fn().mockResolvedValue(movementMock),
+                        findOne: jest.fn().mockResolvedValue(movementMock),
+                        save: jest.fn().mockResolvedValue(movementMock),
+                    }
+                },
+                {
+                    provide: getRepositoryToken(CustomerEntity),
+                    useValue: {
+                        find: jest.fn().mockResolvedValue(customerMock),
+                        findOne: jest.fn().mockResolvedValue(customerMock),
+                        save: jest.fn().mockResolvedValue(customerMock),
+                    }
+                },
+                {
+                    provide: getRepositoryToken(TransferEntity),
+                    useValue: {}
+                },
+                {
+                    provide: getRepositoryToken(AccountEntity),
+                    useValue: {
+                        find: jest.fn().mockResolvedValue(accountMock),
+                        findOne: jest.fn().mockResolvedValue(accountMock[0]),
+                        save: jest.fn().mockResolvedValue(accountMock[0]),
+                    }
+                }
+            ],
+            controllers: [AccountController],
+        }).compile();
 
-    controller = module.get<AccountController>(AccountController);
-  });
+        accountController = module.get<AccountController>(AccountController);
+        accountService = module.get<AccountService>(AccountService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(accountController).toBeDefined();
+    });
+
+    describe('create', () => {
+        it ('should return new account created', async () => {
+            const result = await accountController.create(customerMock)
+
+            expect(result).toBe(accountMock[0])
+        })
+
+        it ('should throw an exception', () => {
+            jest.spyOn(accountService, 'create').mockRejectedValueOnce(new Error())
+            expect(accountService.create(customerMock)).rejects.toThrowError()
+        });
+    })
 });
--- src/api/controllers/account.controller.ts
+++ src/api/controllers/account.controller.ts
@@ -1,51 +1,42 @@
-import {Body, Controller, Get, Param, Post, Res} from '@nestjs/common';
-import {BaseController} from "../utils/BaseController";
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
 import {AccountService} from "../services/account.service";
 import {CustomerEntity} from "../entities/customer.entity";
 
 @Controller('account')
-export class AccountController extends BaseController
+export class AccountController
 {
     constructor(
         private readonly model: AccountService
-    ) {
-        super()
-    }
+    ) {}
 
     @Post()
-    async create(@Body() attributes: CustomerEntity, @Res() res)
+    async create(@Body() attributes: CustomerEntity)
     {
-        const model = await this.model.create(attributes);
-        return this.response(res, model);
+        return await this.model.create(attributes);
     }
 
     @Post(':account_id/credit')
-    async credit(@Param('account_id') account_id: number, @Body() attributes: {value: number}, @Res() res)
+    async credit(@Param('account_id') account_id: number, @Body() attributes: {value: number})
     {
-        const model = await this.model.toCredit({
+        return  await this.model.toCredit({
             value: attributes.value,
             id: account_id
         })
-
-        return this.response(res, model)
     }
 
     @Post(':account_id/debit')
-    async debit(@Param('account_id') account_id: number, @Body() attributes: {value: number}, @Res() res)
+    async debit(@Param('account_id') account_id: number, @Body() attributes: {value: number})
     {
-        const model = await this.model.toDebit({
+        return await this.model.toDebit({
             value: attributes.value,
             id: account_id
         })
 
-        return this.response(res, model)
     }
 
     @Get(':account_id/balance')
-    async balance(@Param('account_id') account_id: number, @Res() res)
+    async balance(@Param('account_id') account_id: number)
     {
-        const model = await this.model.balance(account_id)
-
-        return this.response(res, model)
+        return await this.model.balance(account_id)
     }
 }
