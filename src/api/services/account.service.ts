234b1098e05696405f598652d3600362f3e7aadc
Alexandre Vieira <alexandre@febacapital.com>
2022-03-06 16:22:40 -0300

    test to transfer service

--- src/api/services/account.service.ts
+++ src/api/services/account.service.ts
@@ -1,4 +1,4 @@
-import {Inject, Injectable} from '@nestjs/common';
+import {BadRequestException, Inject, Injectable, NotFoundException} from '@nestjs/common';
 import {getRepository, Repository} from "typeorm";
 import {AccountEntity} from "../entities/account.entity";
 import {CustomerService} from "./customer.service";
@@ -6,6 +6,7 @@ import {CustomerEntity} from "../entities/customer.entity";
 import {MovementService} from "./movement.service";
 import {MovementEntity} from "../entities/movement.entity";
 import {InjectRepository} from "@nestjs/typeorm";
+import {response} from "express";
 
 @Injectable()
 export class AccountService
@@ -105,18 +106,21 @@ export class AccountService
         const model = await this.model
 
         try {
-            await this.customer.create(attributes)
+            const customer = await this.customer.create(attributes)
+            if (customer instanceof Error) {
+                return new Error(customer.message)
+            }
+
+            attributes.id = customer.id
+
         } catch (e) {
-            return e.message
+            return new Error(e.message)
         }
 
-        const accountAttr = {
+        const save = await model.save({
             balance: 0,
             customer_id: attributes.id,
-        } as AccountEntity
-
-
-        const save = await model.save(accountAttr)
+        })
 
         if (!save) {
             return new Error('Error on save account')
